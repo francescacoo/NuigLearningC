3483a831d5fb3fbf4f1c51b9baea04ae3af9f350
francescacoo <francescacoo@gmail.com>
2017-01-21 22:26:26 +0000

    question2.c

--- Question2/question2.c
+++ Question2/question2.c
@@ -3,17 +3,22 @@ Use the constant value 3.14159 for .
 Perform each of these calculations inside the printf statement(s) and use the conversion specifier %f.
 */
 
+// including the standard input output functions
 #include <stdio.h>
+// including the value for PI in the preprocessor directive
 #define PI 3.14159
 
 int main( void)
 {
+	//declare and initialize the variable radius as float
 	float radius;
-
+	// request to enter the value of the radius
 	printf("Enter the radius\n");
+	// retrieve the value as float
 	scanf("%f", &radius);
 
-printf("\nDiameter is %f \nCircumference is %f \nArea is %f \n", radius*2, (2*PI*radius), PI*radius*radius);
+	// perform and print the result of the calculations
+	printf("\nDiameter is %f \nCircumference is %f \nArea is %f \n", radius*2, (2*PI*radius), PI*radius*radius);
 
-		return 0;
+	return 0;
 }
