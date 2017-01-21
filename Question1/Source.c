3b4760829327c22e2f57abaa746e3a069da542ca
francescacoo <francescacoo@gmail.com>
2017-01-21 23:10:37 +0000

    adding input checking

--- Question1/Source.c
+++ Question1/Source.c
@@ -15,36 +15,52 @@ int main(void){
 	int difference;
 	int quotient;
 	int remainder;
+	// used for the if statement to check the input is an integer
+    float b;
+
+
+
 
 	printf("Enter first integer\n");    // request to enter an integer
-	scanf("%d", &integer1);				// retrieve the integer
+	scanf("%f", &b);				// retrieve the integer
+	integer1=(int) b;
+	if(integer1==b){
+		b=0;
+		printf("Enter second integer\n");	// request to enter the second integer
+		scanf("%f", &b);				// retrieve the second integer
+		integer2=(int) b;
+		if(integer1==b){
+			sum = integer1 + integer2;			// perfom the addition and print the result
+			printf("Sum is %d\n", sum);			
 
-	printf("Enter second integer\n");	// request to enter the second integer
-	scanf("%d", &integer2);				// retrieve the second integer
+			product = integer1 * integer2;		// perform multiplication and print result
+			printf("Product is %d\n", product);
 
-
-	sum = integer1 + integer2;			// perfom the addition and print the result
-	printf("Sum is %d\n", sum);			
-
-	product = integer1 * integer2;		// perform multiplication and print result
-	printf("Product is %d\n", product);
-
-	difference = integer1 - integer2;	// perform the subtraction and print the result
-	printf("Difference is %d\n", difference);
+			difference = integer1 - integer2;	// perform the subtraction and print the result
+			printf("Difference is %d\n", difference);
 	
-	// check if the second integer is different from 0 perform and print division and modulus
-	if(integer2 != 0){					
-	quotient = integer1 / integer2;
-	printf("Quotient is %d\n", quotient);
+		// check if the second integer is different from 0 perform and print division and modulus
+			if(integer2 != 0){					
+				quotient = integer1 / integer2;
+				printf("Quotient is %d\n", quotient);
 
-	remainder = integer1%integer2;
-	printf("Remainder is %d\n", remainder);
-	}
-	// if the second integer is 0, print an error message
-	else {
-		printf("I cannot perform the quotient and remainder on 0\n");
-	}
+				remainder = integer1%integer2;
+				printf("Remainder is %d\n", remainder);
+			}
+		// if the second integer is 0, print an error message
+			else {
+				printf("I cannot perform the quotient and remainder on 0\n");
+			}
+		}
+			// closing the if checking for the 2nd number
+		else{
+			printf("The number inserted is not an integer. Exiting.. \n");
+		}	
 
+	}
+	// closing the if checking for the 1st number
+	else{
+		printf("The number inserted is not an integer. Exiting.. \n");
+	}
 	return 0;
-
 }
