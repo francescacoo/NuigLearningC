9d689bdf5e7f8965d1b9dc6a04f877f6a12a3102
francescacoo <francescacoo@gmail.com>
2017-01-26 15:56:51 +0000

    Update source.c

--- week2/Assignment2/question2/source.c
+++ week2/Assignment2/question2/source.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-int counter = 0;
-int number=0;
-int largest=0;
+
 
 int main(void) {
-
+	int counter = 0;
+	int number=0;
+	int largest=0;
+	
 	while(counter <= 10){
 		printf("%s", "Enter the number of units sold: ");
 		scanf("%d", &number);
 		
-	if(number > largest) {
-		largest = number;
+		if(number > largest) {
+			largest = number;
+			}
+			counter++;
 		}
-		counter++;
-	}
 
 	printf("\nThe largest number is: %d \n", largest);
 	return 0;
