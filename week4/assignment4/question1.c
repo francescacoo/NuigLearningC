c459e7bae6246f021f54abb0d239d9a56273bcbe
francescacoo <francescacoo@gmail.com>
2017-02-12 11:56:59 +0000

    update

--- week4/assignment4/question1.c
+++ week4/assignment4/question1.c
@@ -16,28 +16,10 @@ double hypotenuse(double num1, double num2); //function prototype
 
 
 int main(){
-
-	// define all the variables for the 3 triangles
-	double Triangle1_side1=3.0;
-	double Triangle1_side2=4.0;
-
-	double Triangle2_side1=5.0;
-	double Triangle2_side2=12.0;
-
-	double Triangle3_side1=8.0;
-	double Triangle3_side2=15;
-
-	//call the function hyptenuse for each triangle and store the returned values
- 	double Triangle1_hyp=hypotenuse(Triangle1_side1,Triangle1_side2);
-  	double Triangle2_hyp=hypotenuse(Triangle2_side1,Triangle2_side2);
- 	double Triangle3_hyp=hypotenuse(Triangle3_side1,Triangle3_side2);
-
- 	//print the triangles sides and result from the hypotenuse function
- 	printf("Triangle1: side 1 is %.2f, side 2 is %.2f, the hypotenuse is %.2f",Triangle1_side1,Triangle1_side2,Triangle1_hyp);
- 	printf("\nTriangle2: side 1 is %.2f, side 2 is %.2f, the hypotenuse is %.2f",Triangle2_side1,Triangle2_side2,Triangle2_hyp);
- 	printf("\nTriangle3: side 1 is %.2f, side 2 is %.2f, the hypotenuse is %.2f\n",Triangle3_side1,Triangle3_side2,Triangle3_hyp);
-  
- 
+ 	//call the function hypotenuse three times and the result
+ 	printf("Triangle 1: %.2f\n",hypotenuse(3.0,4.0));
+    printf("Triangle 2: %.2f\n",hypotenuse(5.0,12.0));
+    printf("Triangle 3: %.2f\n",hypotenuse(8.0,15));
   return 0;
 }
  
