00907df5b55d5914916c7c9cdce1d0741fccf797
francescacoo <francescacoo@gmail.com>
2017-02-12 20:24:54 +0000

    updated

--- week4/assignment4/question2_1.c
+++ week4/assignment4/question2_1.c
@@ -1,42 +1,63 @@
 #include <stdio.h>
 #include <time.h>
  
-long factorial(int);
- 
+// function prototypes 
+unsigned long long int recursiveFactorial(int); 
+unsigned long long int loopFactorial(int);
+
 int main()
 {
-  int n;
-  long f;
- 
-  printf("Enter an integer to find factorial\n");
-  scanf("%d", &n); 
- 
-  if (n < 0)
-    printf("Negative integers are not allowed.\n");
-  else
-  {
 
-clock_t tic = clock();
+  unsigned long long int f,l;
+  long double e, e2;
 
-    f = factorial(n);
+  e=0.0;
+  e2=0.0;
+  
+  clock_t tic = clock();
 
-clock_t toc = clock();
-
-    printf("Elapsed: %f seconds\n\n", (double)(toc - tic) / CLOCKS_PER_SEC);
-
-    printf("%d! = %ld\n", n, f);
-
-
-
+  for(int i=0; i<20; i++){
+    f = recursiveFactorial(i);
+    printf("%d! = %llu\n", i, f);
+    e += 1.0/f;
+    printf("current e: %Lf\n", e);
   }
+    
+
+  clock_t toc = clock();
+
+  printf("Elapsed: %f seconds\n\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+
+    
+   clock_t tic1 = clock();
+
+  for(int x=0; x<20; x++){
+    l = recursiveFactorial(x);
+    printf("%d! = %llu\n", x, l);
+    e2 += 1.0/l;
+    printf("current e: %Lf\n", e2);
+  }
+    
+
+  clock_t toc1 = clock();
+
+  printf("Elapsed: %f seconds\n\n", (double)(toc1 - tic1) / CLOCKS_PER_SEC);
  
   return 0;
 }
  
-long factorial(int n)
-{
+unsigned long long int recursiveFactorial(int n){
   if (n == 0)
     return 1;
   else
-    return(n * factorial(n-1));
+    return(n * recursiveFactorial(n-1));
+}
+
+unsigned long long int loopFactorial(int n){
+  int c;
+  long fact = 1;
+  for (c = 1; c <= n; c++)
+    fact = fact * c;
+
+  return fact;
 }
